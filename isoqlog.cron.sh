875049dddeaf242ea14274063699ba2d1e7947bc
Eric Shubert <eric@datamatters.us>
2014-04-11 15:16:03 -0700

    Changed location of output files

--- isoqlog.cron.sh
+++ isoqlog.cron.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-confdir=/etc/isoqlog
-
-# Generate the domains file
 ls /home/vpopmail/domains >$confdir/isoqlog.domains
 
-# Execute the program
-isoqlog -f $confdir/isoqlog.conf >/dev/null 2>&1
+isoqlog -f /etc/isoqlog/isoqlog.conf >/dev/null
 
-# Correct permissions
 chown -R apache:apache /usr/share/isoqlog/htdocs
